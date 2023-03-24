16cd05774788dc5e5552cc87919c5f5e4e93c4c7
Kojoblay7 <kngblay@gmail.com>
2023-03-24 05:41:46 -0700

    asgsf|

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - identifies the input which is positive or negative
+ * @i: represents inputs 
+ * Return: always void
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else 
+		printf("%d is zero\n", i);
+}
